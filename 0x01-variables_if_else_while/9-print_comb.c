68afede54e85991b133b71e4d7c2d02c69fce736
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-06-16 07:59:44 -0700

    fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,18 +10,21 @@
  */
 int main(void)
 {
-    int i = 0;
-    while (i < 10)
-    {
-    if (i > 0)
-    {
-    putchar(',');
-    putchar(' ');
-    }
-    putchar('0' + i)
-    i++;
-    }
-    putchar('\n');
-    return (0);
+int i = 0;
+
+while (i < 10)
+{
+if (i > 0)
+{
+putchar(',');
+putchar(' ');
+}
+putchar('0' + i);
+i++;
+}
+
+putchar('\n');
+
+return (0);
 }
 
