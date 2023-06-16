462af08d6b8b883a1947ba91c9adadaa85c26937
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-06-16 07:27:47 -0700

    6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,24 +3,21 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints the single-digit numbers of base 10
- *              starting from 0, followed by a new line.
+ * Description: Prints all single-digit numbers of base 10 starting from 0,
+ *              followed by a new line.
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	putchar('0' + 0);
-	putchar('0' + 1);
-	putchar('0' + 2);
-	putchar('0' + 3);
-	putchar('0' + 4);
-	putchar('0' + 5);
-	putchar('0' + 6);
-	putchar('0' + 7);
-	putchar('0' + 8);
-	putchar('0' + 9);
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
 	putchar('\n');
 
 	return (0);
 }
+
