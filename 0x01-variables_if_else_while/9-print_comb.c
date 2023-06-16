7382c59ceb5cfb1e1d02272c104761466d3d388e
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-06-16 07:55:38 -0700

    betty

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,20 +11,17 @@
 int main(void)
 {
     int i = 0;
-
     while (i < 10)
     {
-        if (i > 0)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-        putchar('0' + i);
-        i++;
+    if (i > 0)
+    {
+    putchar(',');
+    putchar(' ');
     }
-
+    putchar('0' + i)
+    i++;
+    }
     putchar('\n');
-
     return (0);
 }
 
