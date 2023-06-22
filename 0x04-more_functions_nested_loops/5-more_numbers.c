2b490b658cecdf1be4498bfc0e6a83b5db9f646e
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-06-22 12:24:48 -0700

    new output

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,18 +5,21 @@
  */
 void more_numbers(void)
 {
-int i, j;
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j <= 14; j++)
-{
-if (j > 9)
-{
-_putchar((j / 10) + '0');
-}
-_putchar((j % 10) + '0');
-}
-_putchar('\n');
-}
+	int i = 0;
+	int j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			if (j > 9)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
 
