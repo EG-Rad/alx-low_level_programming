97e0db13cc30bbdb889309508f77ceea1d73ee41
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-06-22 14:47:29 -0700

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,8 +9,7 @@ int main(void)
 {
     int i = 1;
 
-    while (i <= 100)
-    {
+    do {
         if (i % 3 == 0 && i % 5 != 0)
             printf("Fizz ");
         else if (i % 3 != 0 && i % 5 == 0)
@@ -21,10 +20,10 @@ int main(void)
             printf("%d ", i);
 
         i++;
-    }
+    } while (i <= 100);
 
     printf("\n");
 
-    return 0;
+    return (0);
 }
 
