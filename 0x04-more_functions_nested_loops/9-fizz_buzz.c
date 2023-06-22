59da343a2db8a7e46b86562f6c373b95aad6e172
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-06-22 14:23:31 -0700

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,30 +7,32 @@
  */
 int main(void)
 {
-    int i;
+        int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 != 0)
+        for (i = 1; i <= 100; ++i)
         {
-            printf(" Fizz");
+                if (i % 3 == 0 && i % 5 != 0 && i != 100)
+                {
+                        printf("Fizz ");
+                }
+                else if (i % 5 == 0 && i % 3 != 0 && i != 100)
+                {
+                        printf("Buzz ");
+                }
+                else if (i % 3 == 0 && i % 5 == 0 && i != 100)
+                {
+                        printf("FizzBuzz ");
+                }
+                else if (i == 100)
+                {
+                        printf("Buzz");
+                }
+                else
+                {
+                        printf("%d ", i);
+                }
         }
-        else if (i % 3 != 0 && i % 5 == 0)
-        {
-            printf(" Buzz");
-        }
-        else if (i % 3 == 0 && i % 5 == 0)
-        {
-            printf(" FizzBuzz");
-        }
-        else
-        {
-            printf("%d ", i);
-        }
-    }
+        printf("\n");
+        return (0);
 
-    printf("\n");
-
-    return (0);
-}
 
