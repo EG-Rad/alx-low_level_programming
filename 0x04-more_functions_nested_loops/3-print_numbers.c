b647d73e603aa4ff1094447a559840067d2eca49
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-06-22 11:44:42 -0700

    putchar troubles

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,12 @@
 /**
  * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
  */
-
 void print_numbers(void)
-
 {
-    int x;
+    char c;
 
-    for (x = '0'; x <= '9'; x++)
-{
-        _putchar(x + '0');
-}
+    for (c = '0'; c <= '9'; c++)
+        _putchar(c);
+
     _putchar('\n');
+}
