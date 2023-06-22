54de9daa37cf9dcedce40a8e6bbd37fe372ff442
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-06-22 12:40:13 -0700

    bettyyy

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * more_numbers - Prints 10 times the numbers from 0 to 14, insert new line.
+ * more_numbers - Prints more numbers
  */
+
 void more_numbers(void)
 {
 	int i, j;
