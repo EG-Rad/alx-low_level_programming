448ad6cf7c72e7a84b716513d9de2fc5a053aa5f
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-06-22 11:30:40 -0700

    1

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,10 @@
 void print_numbers(void)
 
 {
-    int c;
+    int x;
 
-    for (c = '0'; c <= '9'; c++)
+    for (x = '0'; x <= '9'; x++)
 {
-    _putchar(c + '0');
+        _putchar(x + '0');
 }
     _putchar('\n');
-}
