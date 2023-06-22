107b49226dcb0cec5e89c0d29318998d2a571062
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-06-22 12:48:02 -0700

    challenge betty

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
- * more_numbers - Prints more numbers
+ * more_numbers 
  */
-
 void more_numbers(void)
 {
 	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-	for (j = 0; j <= 14; j++)
-	{
-	if (j > 9)
-        {
-	_putchar((j / 10) + '0');
-        }
-	_putchar((j % 10) + '0');
-	}
-	_putchar('\n');
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+
+		_putchar('\n');
 	}
 }
+
