dddcf4ac84248c428a17794219d0a9480ce4c377
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-06-29 07:39:51 -0700

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: Pointer to the capitalized string.
+ */
+char *cap_string(char *str)
+{
+int i, j;
+int capitalize_next = 1;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+str[i] == '}')
+{
+capitalize_next = 1;
+}
+
+else if
+(str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')
+{
+
+if (capitalize_next)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 'a' + 'A';
+}
+capitalize_next = 0;
+}
+}
+}
+
+return (str);
+}
