039e44c5c68809999314424322bd11b86a18f22b
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-06-29 08:03:17 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,34 +8,33 @@
  */
 char *cap_string(char *str)
 {
-int i, j;
-int capitalize_next = 1;
+	int i;
+	int capitalize_next = 1;
 
-for (i = 0; str[i] != '\0'; i++)
-{
-if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-str[i] == '}')
-{
-capitalize_next = 1;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+		    str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		    str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+		    str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+		    str[i] == '}')
+		{
+			capitalize_next = 1;
+		}
+		else if
+		((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			if (capitalize_next)
+			{
+				if (str[i] >= 'a' && str[i] <= 'z')
+				{
+					str[i] = str[i] - 'a' + 'A';
+				}
+				capitalize_next = 0;
+			}
+		}
+	}
+
+	return (str);
 }
 
-else if
-(str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')
-{
-
-if (capitalize_next)
-{
-if (str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] - 'a' + 'A';
-}
-capitalize_next = 0;
-}
-}
-}
-
-return (str);
-}
