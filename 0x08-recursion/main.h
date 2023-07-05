4cf8b1bf6bcdf4686c27f4b2103ddefcf7a34b97
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-07-05 09:34:14 -0700

    5

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,7 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int sqrt_helper(int n, int guess);
 int is_prime_number(int n);
 
 #endif /* MAIN_H */
