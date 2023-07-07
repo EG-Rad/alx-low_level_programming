1db7068c1425deefecc4d21def5da270cc751366
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-07-07 05:30:07 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc; /* Unused parameter */
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
