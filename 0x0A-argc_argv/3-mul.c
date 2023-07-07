300cff8b412aa0a59e0c33cf29a3c880823ce62d
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-07-07 06:13:43 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -23,16 +23,18 @@ int multiply(int a, int b)
  */
 int main(int argc, char *argv[])
 {
+	int num1, num2, result;
+
 	if (argc != 3)
 	{
 	printf("Error\n");
 	return (1);
 	}
 
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
-int result = multiply(num1, num2);
+	result = multiply(num1, num2);
 	printf("%d\n", result);
 
 	return (0);
