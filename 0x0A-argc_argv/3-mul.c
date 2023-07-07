7fa0c8e0b09abed377da5e917fdbd8b488b87a68
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-07-07 05:58:52 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * multiply - Returns the result of multiplying two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of a and b.
+ */
+int multiply(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * main - Multiplies two numbers provided as command-line arguments
+ * and prints the result.
+ * @argc: The argument count.
+ * @argv: The argument vector.
+ *
+ * Return: 0 on success, 1 on error.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
+
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2]);
+
+int result = multiply(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
+
