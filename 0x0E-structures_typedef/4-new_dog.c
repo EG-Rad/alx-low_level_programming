709c8cb7487dd7d12ee7c12a0aeca75d187486a8
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-07-17 07:15:19 -0700

    4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,53 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - Creates a new dog_t and stores a copy of name and owner
+ * @name: Pointer to a character array for the dog's name
+ * @age: Age of the dog
+ * @owner: Pointer to a character array for the owner's name
+ *
+ * Return: Pointer to the newly created dog_t, or NULL if failed
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *name_copy, *owner_copy;
+	int name_len, owner_len;
+
+	/* Calculate the lengths of name and owner */
+	name_len = strlen(name);
+	owner_len = strlen(owner);
+
+	/* Allocate memory for the new dog_t structure */
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	/* Allocate memory for the name copy */
+	name_copy = malloc((name_len + 1) * sizeof(char));
+	if (name_copy == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	/* Allocate memory for the owner copy */
+	owner_copy = malloc((owner_len + 1) * sizeof(char));
+	if (owner_copy == NULL)
+	{
+		free(name_copy);
+		free(new_dog);
+		return (NULL);
+	}
+
+	/* Copy the name and owner to the new memory locations */
+	strcpy(name_copy, name);
+	strcpy(owner_copy, owner);
+
+	/* Initialize the new dog_t structure */
+	init_dog(new_dog, name_copy, age, owner_copy);
+
+	return (new_dog);
+}
