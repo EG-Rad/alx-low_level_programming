d362d3700d2d35bed849ca4073a2e4738d20ba49
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-07-17 07:29:01 -0700

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+/**
+ * free_dog - Frees the memory allocated for a dog_t structure
+ * @d: Pointer to the dog_t structure to be freed
+ *
+ * Return: None (void)
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
