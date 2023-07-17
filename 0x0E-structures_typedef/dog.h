bc7b45d67b7dbf4740dff608506b0c61bb573543
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-07-17 07:03:14 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure representing a dog
+ * @name: Pointer to a character array for the dog's name
+ * @age: Age of the dog
+ * @owner: Pointer to a character array for the owner's name
+ *
+ * Description: This structure represents a dog, including its name, age, and owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
