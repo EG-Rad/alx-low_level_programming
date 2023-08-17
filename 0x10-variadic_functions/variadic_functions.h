e3d66f83742cd64504646a6fe6ee6891a79c567a
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-08-17 08:04:38 -0700

    .h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,14 +3,14 @@
 
 #include <stdarg.h>
 
+#define UNUSED(x) (void)(x)
+
+/* Function prototypes */
 int _putchar(char c);
-
 int sum_them_all(const unsigned int n, ...);
-
 void print_numbers(const char *separator, const unsigned int n, ...);
-
 void print_strings(const char *separator, const unsigned int n, ...);
-
 void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
+
