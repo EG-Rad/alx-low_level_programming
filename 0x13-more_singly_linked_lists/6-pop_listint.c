53162dd09eab98e982b174e6be5e0284fcfa8e89
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-08-28 09:09:42 -0700

    6-

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,13 +9,16 @@
  */
 int pop_listint(listint_t **head)
 {
-	if (*head == NULL)
-		return (0);
+	int data = 0;
 
-	int data = (*head)->n;
-	listint_t *temp = *head;
-	*head = (*head)->next;
-	free(temp);
+	if (*head != NULL)
+	{
+		data = (*head)->n;
+		listint_t *temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
 
 	return (data);
 }
+
