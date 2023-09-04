95c6d5d63e47b19067b0a0696431185eee3a3cd2
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-09-04 04:28:35 -0700

    1-

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/stat.h>
+#include "main.h"
 
 /**
- * create_file - Creates a file and writes text content to it
- * @filename: The name of the file to create
- * @text_content: A NULL-terminated string to write to the file
+ * create_file - creates a file
+ * @filename: the name of the file to create
+ * @text_content: a NULL terminated string to write to the file
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, write_result;
-	mode_t permissions = S_IRUSR | S_IWUSR; /* rw------- */
+	int fd;
+	ssize_t n_written;
+	mode_t mode;
 
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, permissions);
-	if (file_descriptor == -1)
+	mode = S_IRUSR | S_IWUSR;
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		write_result = write(file_descriptor, text_content, strlen(text_content));
-		if (write_result == -1)
+		n_written = write(fd, text_content, _strlen(text_content));
+		if (n_written == -1)
 		{
-			close(file_descriptor);
+			close(fd);
 			return (-1);
 		}
 	}
 
-	close(file_descriptor);
+	close(fd);
+
 	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to measure
+ *
+ * Return: the length of the string
+ */
+size_t _strlen(char *s)
+{
+	size_t len;
+
+	for (len = 0; s[len]; len++)
+		;
+
+	return (len);
 }
 
