0f4d43953e0da6da1982ee543c188f7dc189c4e7
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-09-04 05:26:39 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include "main.h"
 
 int open_and_copy_file(const char *source, const char *destination);
 void print_error_and_exit(const char *message, int exit_code);
@@ -20,16 +21,17 @@ int open_destination_file(const char *destination, mode_t permissions);
  */
 int main(int argc, char *argv[])
 {
-    if (argc != 3) {
-        print_error_and_exit("Usage: cp file_from file_to\n", 97);
-    }
+	if (argc != 3)
+	{
+		print_error_and_exit("Usage: cp file_from file_to\n", 97);
+	}
 
-    if (open_and_copy_file(argv[1], argv[2]) != 0)
-    {
-        return (1);
-    }
+	if (open_and_copy_file(argv[1], argv[2]) != 0)
+	{
+		return (1);
+	}
 
-    return (0);
+	return (0);
 }
 
 /**
@@ -41,29 +43,32 @@ int main(int argc, char *argv[])
  */
 int open_and_copy_file(const char *source, const char *destination)
 {
-    int fd_from, fd_to;
+	int fd_from, fd_to;
 
-    fd_from = open_source_file(source);
-    fd_to = open_destination_file(destination, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd_from = open_source_file(source);
 
-    if (fd_from == -1 || fd_to == -1)
-    {
-        return (1);
-    }
+	fd_to = open_destination_file(destination,
+	S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 
-    if (read_and_write_files(fd_from, fd_to) != 0)
-    {
-        return (1);
-    }
+	if (fd_from == -1 || fd_to == -1)
+	{
+		return (1);
+	}
 
-    close(fd_from);
-    close(fd_to);
+	if (read_and_write_files(fd_from, fd_to) != 0)
+	{
+		return (1);
+	}
 
-    return (0);
+	close(fd_from);
+	close(fd_to);
+
+	return (0);
 }
 
 /**
- * read_and_write_files - Reads from the source file and writes to the destination file.
+ * read_and_write_files - Reads from the source file
+ * and writes to the destination file.
  * @fd_from: File descriptor for the source file.
  * @fd_to: File descriptor for the destination file.
  *
@@ -71,24 +76,24 @@ int open_and_copy_file(const char *source, const char *destination)
  */
 int read_and_write_files(int fd_from, int fd_to)
 {
-    ssize_t read_result, write_result;
-    char buffer[1024];
+	ssize_t read_result, write_result;
+	char buffer[1024];
 
-    while ((read_result = read(fd_from, buffer, 1024)) > 0)
-    {
-        write_result = write(fd_to, buffer, read_result);
-        if (write_result == -1)
+	while ((read_result = read(fd_from, buffer, 1024)) > 0)
 	{
-            print_error_and_exit("Error: Can't write to file", 99);
-        }
-    }
+		write_result = write(fd_to, buffer, read_result);
+		if (write_result == -1)
+		{
+			print_error_and_exit("Error: Can't write to file", 99);
+		}
+	}
 
-    if (read_result == -1)
-    {
-        print_error_and_exit("Error: Can't read from file", 98);
-    }
+	if (read_result == -1)
+	{
+		print_error_and_exit("Error: Can't read from file", 98);
+	}
 
-    return (0);
+	return (0);
 }
 
 /**
@@ -99,12 +104,13 @@ int read_and_write_files(int fd_from, int fd_to)
  */
 int open_source_file(const char *source)
 {
-    int fd_from = open(source, O_RDONLY);
-    if (fd_from == -1)
-    {
-        print_error_and_exit("Error: Can't read from file", 98);
-    }
-    return (fd_from);
+	int fd_from = open(source, O_RDONLY);
+
+	if (fd_from == -1)
+	{
+		print_error_and_exit("Error: Can't read from file", 98);
+	}
+	return (fd_from);
 }
 
 /**
@@ -116,12 +122,13 @@ int open_source_file(const char *source)
  */
 int open_destination_file(const char *destination, mode_t permissions)
 {
-    int fd_to = open(destination, O_CREAT | O_WRONLY | O_TRUNC, permissions);
-    if (fd_to == -1)
-    {
-        print_error_and_exit("Error: Can't write to file", 99);
-    }
-    return (fd_to);
+	int fd_to = open(destination, O_CREAT | O_WRONLY | O_TRUNC, permissions);
+
+	if (fd_to == -1)
+	{
+		print_error_and_exit("Error: Can't write to file", 99);
+	}
+	return (fd_to);
 }
 
 /**
@@ -131,7 +138,7 @@ int open_destination_file(const char *destination, mode_t permissions)
  */
 void print_error_and_exit(const char *message, int exit_code)
 {
-    dprintf(STDERR_FILENO, "%s\n", message);
-    exit(exit_code);
+	dprintf(STDERR_FILENO, "%s\n", message);
+	exit(exit_code);
 }
 
