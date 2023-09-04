4bad7697aaf68766a8acf90d72b682f5fbff5117
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-09-04 04:24:59 -0700

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+/**
+ * create_file - Creates a file and writes text content to it
+ * @filename: The name of the file to create
+ * @text_content: A NULL-terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor, write_result;
+	mode_t permissions = S_IRUSR | S_IWUSR; /* rw------- */
+
+	if (filename == NULL)
+		return (-1);
+
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, permissions);
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		write_result = write(file_descriptor, text_content, strlen(text_content));
+		if (write_result == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
+
+	close(file_descriptor);
+	return (1);
+}
+
